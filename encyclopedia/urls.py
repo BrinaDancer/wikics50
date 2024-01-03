b2ad4c4bac496ff1154d886b23f8cf76109c1e6d
Sabrina Davidson <65583836+BrinaDancer@users.noreply.github.com>
2024-01-03 13:16:22 -0500

    Add files via upload

---
+++ encyclopedia/urls.py
@@ -0,0 +1,13 @@
+from secrets import randbelow
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("wiki/<title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("random", views.random_page, name="random"),
+    path("create", views.create, name="create"),
+    path("edit/<title>", views.edit, name="edit"), 
+]
